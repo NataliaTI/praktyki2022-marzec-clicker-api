2a140b81f29d7e1a6a467b80b48a543dd18b37fc
KamilZas <zkamil@onet.pl>
2022-03-15 14:02:11 +0100

    #1 wstepna struktura bazy danych oraz polaczenie z baza danych

--- bazadanych.php
+++ bazadanych.php
@@ -1,7 +1,7 @@
 <?php
 $mysqli = new mysqli("localhost", "root","ZAQ!2wsx","clicker");
 if ($mysqli -> connect_error){
-    echo "<br>Failed to connect to DB:<br>" . $mysqli->connect_error;
+    echo "<br>Failed to connectt to DB:<br>" . $mysqli->connect_error;
 exit();
 }
 else echo "<br>Connected<br>";
