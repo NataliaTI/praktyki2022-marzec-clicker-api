4f2c4327d42b9a814cb5ec3029c51bbd9c0571dd
KamilZas <zkamil@onet.pl>
2022-03-18 10:38:19 +0100

    #2 poprawienie czytelnosci oraz usuniecie public

--- dataBaseInterface.php
+++ dataBaseInterface.php
@@ -1,84 +1,103 @@
 <?php
 
-// nawiazywanie polaczenia z baza danych
+/* nawiazywanie polaczenia z baza danych */
 function openCon()
  {
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "ZAQ!2wsx";
  $db = "clicker";
+
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
  return $conn;
  }
- //zamykanie polaczenia z baza danych
+
+ /* zamykanie polaczenia z baza danych */
 function closeCon($conn)
  {
  $conn -> close();
  }
-//tworzenie polecenia sql do bazy danych
- function typeData($dates){
-$collect = "SELECT $dates FROM dataTable";
-return $collect;
- }
-//pobieranie danych z bazy danych
- function collectData($tab){
- $query = $tab;
-if ($result = mysqli_query(openCon(), $query)) {
 
-    /* fetch associative array */
-    while ($row = mysqli_fetch_assoc($result)) {
+/* tworzenie polecenia sql do bazy danych */
+ function typeData($dates)
+ {
+    $collect = "SELECT $dates FROM dataTable";
+
+    return $collect;
+ }
+
+ /* pobieranie danych z bazy danych */
+  function collectData($tab){
+
+    $query = $tab;
+
+    if ($result = mysqli_query(openCon(), $query)) 
+    {
+
+    while ($row = mysqli_fetch_assoc($result))
+     {
         printf ("%s (%s)\n", $row["$tab"]);
     }
 
-    /* free result set */
+  
     mysqli_free_result($result);
 }
 
-/* close connection */
+/* zamkniecie polaczenia */
 closeCon(openCon()); 
  }
- // zapisywanie do bazy danych 
+ /* zapisywanie do bazy danych */
 function insertData($insertData){
+
     $sql = "INSERT INTO dataBase VALUES ($insertData)";
-if(mysqli_query(openCon(), $sql)){
+
+    if(mysqli_query(openCon(), $sql)){
+
     echo "Records inserted successfully.";
-} else{
+}
+ else{
+    
     echo "ERROR: Could not able to execute $sql. " . mysqli_error(openCon());
+
 }
 }
-//szukanie po id
-public function find($id)
+/* szukanie po id */
+function find($id)
 {
+
     $statement = "
-        SELECT 
-            id, userid, status
-        FROM
-            dataTable
-        WHERE id = ?;
-    ";
+        SELECT id, userid, status FROM dataTable WHERE id = $id";
+    
     if ($result = mysqli_query(openCon(), $statement)) {
 
-        /* fetch associative array */
         while ($row = mysqli_fetch_assoc($result)) {
+    
             printf ("%s (%s)\n", $row["$statement"]);
         }
-    
-        /* free result set */
+
         mysqli_free_result($result);
     }
 }
 
-public function delete($id)
+function delete($id)
 {
+
     $statement = "
         DELETE FROM dataTable
         WHERE id = :id;
     ";
+
     if (mysqli_query(openCon(), $statement)) {
+    
         echo "Record deleted successfully";
-    } else {
-      echo "Error deleting record: " . mysqli_error($conn);
+
+    } 
+    
+    else {
+    
+        echo "Error deleting record: " . mysqli_error($conn);
+    
     }
     }
 ?>
