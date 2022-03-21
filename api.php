e1ca93ee2a2a58e9da5ad601d1405a521e4cefa3
TestkaJakub <testkajakub@gmail.com>
2022-03-21 13:14:47 +0100

    #3 Generacja JWT

--- api.php
+++ api.php
@@ -1,17 +1,38 @@
 <?php
-header("Content-Type:application/json");
-
 /*
-const JWT_SECRET_KEY = 'wekfuhweuoghjrwojg';
+header("Content-Type:application/json");
+date_default_timezone_set('Europe/Warsaw');
+$today = date('Y-m-d H:i:s');
+//echo $today;
+$headers = array('alg'=>'HS256','typ'=>'JWT');
+$payload = array('usi'=>rand(1, 100000000000000000),'gat'=>$today);
 
-function genAccessToken() {
-  const tokenPayload = { user_id };
-  const accessToken = jwt.sign(tokenPayload, JWT_SECRET_KEY);
-  return accessToken;
+$jwtGen = generate_jwt($headers, $payload);
+
+echo $jwtGen;
+function generate_jwt($headers, $payload, $secret = 'aabXXryuOOpe342bn24ldaFFGooopffgde45') {
+	$headers_encoded = base64url_encode(json_encode($headers));
+	
+	$payload_encoded = base64url_encode(json_encode($payload));
+	
+	$signature = hash_hmac('SHA256', "$headers_encoded.$payload_encoded", $secret, true);
+	$signature_encoded = base64url_encode($signature);
+	
+	$jwt = "$headers_encoded.$payload_encoded.$signature_encoded";
+	
+	return $jwt;
 }
-$user_id = genAccessToken();
-response($user_id);
-*/
+function base64url_encode($str) {
+    return rtrim(strtr(base64_encode($str), '+/', '-_'), '=');
+}
+
+include('dataBaseInterface.php');
+$result = mysqli_query(
+    openCon(),
+    insertData($jwtGen.' WHERE user_id')
+    //"INSERT INTO `dataTable` (user_id) VALUES ($jwt)"
+);
+/*
 if(isset($_GET['user_id']) && $_GET['user_id']!=""){
     include('dataBaseInterface.php');
     $user_id=$_GET['user_id'];
@@ -29,10 +50,10 @@ if(isset($_GET['user_id']) && $_GET['user_id']!=""){
     }else{
         header("HTTP/1.1 404 Not Found");
     }
-
     function response($user_id, $status){
         $response['user_id'] = $user_id;
         $response['status'] = $status;
         $json_response = json_encode($response);
         echo $json_response; 
     }
+    */
