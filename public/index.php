fd467578309a1226f54d324aceaf088512c53de0
TestkaJakub <testkajakub@gmail.com>
2022-03-31 13:11:32 +0200

    header access-control-allow-methods

--- public/index.php
+++ public/index.php
@@ -1,14 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-<<<<<<< Updated upstream
-=======
 header("Access-Control-Allow-Headers: *");
-
+header('Access-Control-Allow-Methods: GET, PUT, OPTIONS');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
->>>>>>> Stashed changes
 require(__DIR__.'/../src/library/router.php');
 //require(__DIR__.'/../src/endpoint/writeAndRead.php');  
 //__DIR__.dat();
