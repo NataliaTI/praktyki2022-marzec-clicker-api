c95e4c5e5128e5d67ce43ddcaf5f8481be00227e
TestkaJakub <testkajakub@gmail.com>
2022-03-31 13:01:37 +0200

    konflikty3

--- public/index.php
+++ public/index.php
@@ -1,20 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-=======
 header("Access-Control-Allow-Headers: *");
 
->>>>>>> develop
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
->>>>>>> develop
 require(__DIR__.'/../src/library/router.php');
 //require(__DIR__.'/../src/endpoint/writeAndRead.php');  
 //__DIR__.dat();
