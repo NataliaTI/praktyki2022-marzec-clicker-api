41c9688f3ebfd780a19190697e6d58dd358c651d
TestkaJakub <testkajakub@gmail.com>
2022-04-01 12:35:20 +0200

    test

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header('Access-Control-Allow-Methods: GET, PUT, OPTIONS');
-header("Referrer-Policy: unsafe-url");
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
